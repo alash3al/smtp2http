85b8781344a0ab3caba49a20ee20f8c37e3977c8
Mohamed Al Ashaal <m7medalash3al@gmail.com>
2018-11-12 22:07:41 +0200

    added .gitignore

--- main.go
+++ main.go
@@ -13,5 +13,10 @@ func main() {
 		Handler:     handler,
 		Name:        *flagServerName,
 	}
+	fmt.Println("start the smtp server on address: ", *flagListenAddr)
+	fmt.Println("specified maximum body size: ", *flagMaxMessageSize, " bytes")
+	fmt.Println("specified server name: ", *flagServerName)
+	fmt.Println("specified webhook: ", *flagWebhook)
+	fmt.Println("validating the incoming FROM header: ", *flagStrictValidation)
 	fmt.Println(srv.ListenAndServe())
 }
